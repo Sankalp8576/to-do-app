7e95cdc19fce8cac0fd12490c887d5142589e959
Sankalp8576 <72309459+Sankalp8576@users.noreply.github.com>
2021-08-09 11:50:37 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const app = express();
+const dotenv = require(“dotenv”);
+const mongoose = require("mongoose");
+
+dotenv.config();
+
+app.use("/static",express.static("public"))
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {TodoTask.find({}
+     (err, tasks) => {res.render("todo.ejs", { todoTasks: tasks })
+     ;}
+     );});
+
+app.listen(3000, () => console.log("Server Up and running"));
+app.set("view engine", "ejs");
+
+app.use(express.urlencoded({ extended: true }));
+
+app.post('/',async (req, res) => {const todoTask = new TodoTask({content: req.body.content});
+try {await todoTask.save();res.redirect("/");
+} catch (err) {res.redirect("/");
+}});
+
+
+
+
+
+mongoose.set("useFindAndModify", false);mongoose.connect(process.env.DB_CONNECT,
+     { useNewUrlParser: true }, () => {console.log("Connected to db!");app.listen(3000, (
+
+     ) => console.log("Server Up and running"));
+    });
